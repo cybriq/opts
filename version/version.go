56c39c061402b71f4d446519d8eea67f7033a124
David Vennik <stalker.loki@protonmail.ch>
2021-05-17 07:42:00 +0300

    updating

--- version/version.go
+++ version/version.go
@@ -7,17 +7,18 @@ import (
 )
 
 var (
+
 	// URL is the git URL for the repository
 	URL = "github.com/cybriq/opts"
 	// GitRef is the gitref, as in refs/heads/branchname
 	GitRef = "refs/heads/main"
 	// GitCommit is the commit hash of the current HEAD
-	GitCommit = "172946059ee2ff3cd93a90b40632d7e67848dea1"
+	GitCommit = "86d39c59fa7401ae57e83a04bf32d36a8b125211"
 	// BuildTime stores the time when the current binary was built
-	BuildTime = "2021-05-16T18:55:44+03:00"
+	BuildTime = "2021-05-17T07:39:42+03:00"
 	// Tag lists the Tag on the build, adding a + to the newest Tag if the commit is
 	// not that commit
-	Tag = "v0.0.13"
+	Tag = "v0.0.14+"
 	// PathBase is the path base returned from runtime caller
 	PathBase = "/home/loki/src/github.com/cybriq/opts/"
 	// Major is the major number from the tag
@@ -25,7 +26,7 @@ var (
 	// Minor is the minor number from the tag
 	Minor = 0
 	// Patch is the patch version number from the tag
-	Patch = 13
+	Patch = 14
 	// Meta is the extra arbitrary string field from Semver spec
 	Meta = ""
 )
@@ -34,11 +35,11 @@ var (
 func Get() string {
 	return fmt.Sprint(
 		"\nRepository Information\n"+
-			"\tGit repository: "+URL+"\n",
+		"\tGit repository: "+URL+"\n",
 		"\tBranch: "+GitRef+"\n"+
-			"\tCommit: "+GitCommit+"\n"+
-			"\tBuilt: "+BuildTime+"\n"+
-			"\tTag: "+Tag+"\n",
+		"\tCommit: "+GitCommit+"\n"+
+		"\tBuilt: "+BuildTime+"\n"+
+		"\tTag: "+Tag+"\n",
 		"\tMajor:", Major, "\n",
 		"\tMinor:", Minor, "\n",
 		"\tPatch:", Patch, "\n",
