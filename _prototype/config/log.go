d8d945c741b7b3f30db67a03debac72ec68a22a1
Loki <stalker.loki@protonmail.ch>
2021-08-20 12:37:33 +0300

    wrapped up prototype so it isnt picked up by build commands

--- _prototype/config/log.go
+++
@@ -1,9 +0,0 @@
-package config
-
-import (
-	"github.com/p9c/p9/pkg/log"
-	"github.com/p9c/p9/version"
-)
-
-var subsystem = log.AddLoggerSubsystem(version.PathBase)
-var F, E, W, I, D, T log.LevelPrinter = log.GetLogPrinterSet(subsystem)
--- _prototype/podcmds/log.go
+++
@@ -1,9 +0,0 @@
-package podcmds
-
-import (
-	_l "github.com/cybriq/log"
-
-	"github.com/cybriq/opts/version"
-)
-
-var log = _l.Get(_l.Add(version.PathBase))
